b0779b58413e03212314d50fa40f847dc01a9034
Andrés Ruiz Peñuela <arp00048@red.ujaen.es>
2017-12-10 22:45:02 +0100

    Casi terminado

--- SSMM1718_PRACTICA02_GR01/src/main/java/es/ujaen/ssmm1718_practica02_gr01_servidor/HomeController.java
+++ SSMM1718_PRACTICA02_GR01/src/main/java/es/ujaen/ssmm1718_practica02_gr01_servidor/HomeController.java
@@ -1,7 +1,9 @@
 package es.ujaen.ssmm1718_practica02_gr01_servidor;
 
+import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -11,10 +13,16 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import es.ujaen.ssmm1718.practica02_gr01_dispositivos.DaoDispositivos;
+import es.ujaen.ssmm1718.practica02_gr01_dispositivos.DtoDispositivos;
+import es.ujaen.ssmm1718_practica02_gr01_usuarios.DaoUsuario;
+import es.ujaen.ssmm1718_practica02_gr01_usuarios.DtoUsuario;
 
 /**
  * Handles requests for the application home page.
@@ -24,6 +32,17 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	private static final String [] USER = {"Andres","123"};
+	private static String [] PUNTO1 = {"1","ON"};
+	private static String [] PUNTO2 = {"2","ON"};
+	private static String [] PUNTO3 = {"3","OFF"};
+	private static int EXPIREDMS = 3600000;
+	private String sesionID;
+	@Autowired
+	DaoUsuario daoUsser;
+	
+	@Autowired
+	DaoDispositivos daoDispositivos;
+	
 	//Aviso de errores
 	private static final String [] ERRCLIENTE ={"401:Unauthorized","403 - Forbidden","423:Locked"};
 		//Errores del cliente:
@@ -55,41 +74,37 @@ public class HomeController {
 			System.out.println("Servidor: "+request.getParameter("usuario"));
 			String usuario = request.getParameter("usuario");
 			String pass = request.getParameter("clave");
-			if(usuario.equals(USER[0]) && pass.equals(USER[1])){
+			DtoUsuario dtoUsser = daoUsser.verUsuario(usuario);
+			
+			if(dtoUsser!=null && pass.equals(dtoUsser.getPass())){
+				if(pass.equals(dtoUsser.getPass())){
+					System.out.println("a: "+pass+", b: "+dtoUsser.getPass());
+					HttpSession session;
+					//Creamos la sesión
+					session = request.getSession(true);
+					//Añadimos el usuario en la sesión
+					session.setAttribute ("usuario",usuario);
+					sesionID = session.getId(); //Guardamos el identificador de la sesión, dado que con android no la guarda.
+					
+					//Establecemos la expiración de la sesión a 1 hora
+					
+					session.setMaxInactiveInterval(EXPIREDMS/1000);
+					
+					
+					//Devolvemos la fecha de expiracion de la sesión
+					Long horaMilis = new Date().getTime();//Obtenemos la hora
+			        //Long sessionExpired = horaMilis + (Long) incremetoMilis;
+			        Timestamp fecha = new Timestamp(horaMilis);//Lo pasmaos al fotmarto "Timestamp"
+			        fecha.setTime(fecha.getTime()+EXPIREDMS); //Aumentamos una hora
+					
+			        //Genermoas la respuesta del servidor
+					resultado = "SESSION-ID="+session.getId()+"&EXPIERD="+fecha;
+				}else{
+					resultado = this.ERRCLIENTE[0];	
+				}
 				
-				
-				HttpSession session;
-				//Creamos la sesión
-				session = request.getSession(true);
-				//Añadimos el usuario en la sesión
-				session.setAttribute ("usuario",usuario);
-				//Establecemos la expiración de la sesión a 1 hora
-				session.setMaxInactiveInterval(3600);
-				
-				
-				//Devolvemos la fecha de expiracion de la sesión
-				Calendar cal = Calendar.getInstance();
-				cal.setTime(new Date());
-				
-				//Aumentamos unara hora
-				Date tempDate = cal.getTime();
-				System.out.println("Fecha actual: " + tempDate);
-				
-				//Aumentamos una 1, a la actual
-				//cal.set(Calendar.HOUR, cal.get(Calendar.HOUR)+ 1);
-				cal.set(Calendar.MINUTE,cal.get(Calendar.MINUTE)+2); // + 15 minutos
-				
-				tempDate = cal.getTime();
-				System.out.println("Hora modificada: " + tempDate);
-				
-				//Formateamos
-				SimpleDateFormat dt1 = new SimpleDateFormat("HH:mm:ss");
-		        System.out.println(dt1.format(tempDate));
-				
-		        //Genermoas la respuesta del servidor
-				resultado = "SESSION-ID="+session.getId()+"&EXPIERD="+dt1.format(tempDate);
 			}else{
-				resultado = "Error en la autenticacion";
+				resultado = this.ERRCLIENTE[0];
 			}
 			
 		}
@@ -102,5 +117,93 @@ public class HomeController {
 		
 		
 	}
+	
+	//Borrar
+	@RequestMapping(value ={"/hola"}, method = {RequestMethod.GET,RequestMethod.POST})
+	public String home2(HttpServletRequest request, Model model) {
+		return "home";
+	}
+	
+	//
+	@RequestMapping(value ={"/dispositivos"}, method = {RequestMethod.GET,RequestMethod.POST})
+	public String dispositivos (HttpServletRequest request, Model model) {
+		logger.info("Viendo los dispositivos");
+		//Declaración de variables
+		String resultado; //Contiene la respuesta como resultado de ejecutar la petición.
+
+		//Extremos los datos y los procesamos
+		if(request.getParameter("usuario")==null ||  request.getParameter("sessionID")==null){
+			resultado = this.ERRCLIENTE[1];
+		}else{
+			
+			try{
+				System.out.println("Sesion registrada: "+sesionID);
+				System.out.println("Session recibida: "+request.getParameter("sessionID"));
+				if(sesionID.equals(request.getParameter("sessionID"))){
+					System.out.println("entro");
+					ArrayList<DtoDispositivos> listaDispositivos = daoDispositivos.verDispositivo();
+					System.out.println("Lista: "+listaDispositivos.get(0).getId());
+					resultado = "lista=";
+					//Generamos la respuesta para devolver la lista de productos
+					for(int i=0;i<listaDispositivos.size();i++){
+						resultado=resultado+listaDispositivos.get(i).getLocalizacion()
+								+"@"+listaDispositivos.get(i).isEstado();
+						if(i<(listaDispositivos.size()-1)){
+							resultado=resultado+"&";
+						}
+					}
+					
+				}else{
+					resultado = "Sesion erronea";
+				}
+			}catch(Exception e){
+				System.out.println("Excepcion: "+e);
+				resultado = "Error al conectar a la BBDD";
+			}
+			
+		}
+		//Agregamos el resultado a la respuesta.
+		model.addAttribute("resultado",resultado);
+		return VISTAS[0];
+	}
+	
+	//Camibar estado
+	@RequestMapping(value ={"/dispositivo"}, method = {RequestMethod.GET,RequestMethod.POST})
+	public String dispositivo (HttpServletRequest request, Model model) {
+		logger.info("Viendo los dispositivos");
+		//Declaración de variables
+		String resultado; //Contiene la respuesta como resultado de ejecutar la petición.
+
+		//Extremos los datos y los procesamos
+		if(request.getParameter("usuario")==null ||  request.getParameter("sessionID")==null
+				|| request.getParameter("estado")==null || request.getParameter("localizacion")==null){
+			resultado = this.ERRCLIENTE[1];
+		}else{
+			
+			try{
+				
+				if(sesionID.equals(request.getParameter("sessionid"))){
+					Boolean resul =daoDispositivos.cambiarEstado(request.getParameter("localizacion"),
+							Boolean.parseBoolean(request.getParameter("estado")));
+					if(resul){
+						resultado = "ok";
+					}else{
+						resultado = "no ok";
+					}
+					
+				}else{
+					resultado = "Sesion erronea";
+				}
+			}catch(Exception e){
+				System.out.println("Excepcion: "+e);
+				resultado = "Error al conectar a la BBDD";
+			}
+			
+		}
+		//Agregamos el resultado a la respuesta.
+		model.addAttribute("resultado",resultado);
+		return VISTAS[0];
+	}
+	
 	
 }
